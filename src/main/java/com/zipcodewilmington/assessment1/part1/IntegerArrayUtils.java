6b357f5f840204e5e959448fbdfecd9926b7a96c
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-18 09:42:18 -0500

    second commit, int array util

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
@@ -9,7 +9,13 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray`
      */
     public static Integer getSum(Integer[] intArray) {
-        return null;
+        Integer answer = 0;
+
+        for (int i = 0; i < intArray.length; i++) {
+            answer += intArray[i];
+
+        }
+        return answer;
     }
 
     /**
@@ -17,7 +23,12 @@ public class IntegerArrayUtils {
      * @return the product of `intArray`
      */
     public static Integer getProduct(Integer[] intArray) {
-        return null;
+        Integer answer = 1;
+
+        for (int i = 0; i < intArray.length; i++) {
+            answer *= intArray[i];
+        }
+        return answer;
     }
 
     /**
@@ -25,6 +36,15 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray` divided by number of elements in `intArray`
      */
     public static Double getAverage(Integer[] intArray) {
-        return null;
+        Double answer = 0.0;
+
+        for (int i = 0; i < intArray.length; i++) {
+            answer += intArray[i];
+            //System.out.println(answer);
+
+        }
+        answer = answer / intArray.length;
+
+        return answer;
     }
 }
